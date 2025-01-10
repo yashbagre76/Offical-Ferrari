d8f50e5973cfa844e3dad90e38d841a779960fb9
Yash Bagre <yashbagre603@gmail.com>
2025-01-10 19:36:15 +0530

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>hello</h1>
